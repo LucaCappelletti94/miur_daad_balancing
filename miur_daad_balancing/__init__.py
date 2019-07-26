61eeb858d90c755d5350c37c2a08c68ff6f2d56b
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-07-26 16:31:17 +0200

    Introduced callback

--- miur_daad_balancing/__init__.py
+++ miur_daad_balancing/__init__.py
@@ -2,7 +2,8 @@ from .utils import get_classes, load_balanced, load_full_balanced
 from .umbalanced import umbalanced
 from .balanced import balanced
 from .full_balanced import full_balanced
+from .get_callback import get_callback
 
 __all__ = [
-    "load_balanced", "load_full_balanced", "get_classes", "umbalanced", "balanced", "full_balanced"
+    "load_balanced", "load_full_balanced", "get_classes", "umbalanced", "balanced", "full_balanced", "get_callback"
 ]
